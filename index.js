a4f070c61d5d918d2d1a0190981d7bfa941b0736
patrickmouse <patrick@mouse.org>
2016-05-20 20:59:56 -0400

    css for emoji size

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req,res){
 	res.send('<html><head>' +
 			'<title>🍕🍰</title>' +
-			'<style>#pc { font-size: 300px; text-align: center; margin-top: 10%;</style>' +
+			'<style>#pc { font-size: 300px; text-align: center; margin-top: 10%;} #pc::after {content: \"🍕🍰\"; font-size: 300px; display: block;}</style>' +
 			'</head>'+
-			'<body><div id=\'pc\'>🍕🍰</div></body></html>')
+			'<body><div id=\'pc\'></div></body></html>')
 });
 
 app.listen(port, function(){
